fe26044e672fb47e02c19216f5a25d0befe31270
karol-202 <karoljurski1@gmail.com>
2016-09-13 21:20:16 +0200

    Fix toolbar and add Firebase analytics.

--- app/src/main/java/pl/karol202/smartcontrol/ActivityMain.java
+++ app/src/main/java/pl/karol202/smartcontrol/ActivityMain.java
@@ -1,11 +1,14 @@
 package pl.karol202.smartcontrol;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class ActivityMain extends AppCompatActivity
 {
+	private FirebaseAnalytics firebaseAnalytics;
+	
 	private Toolbar toolbar;
 	
 	@Override
@@ -13,6 +16,8 @@ public class ActivityMain extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		firebaseAnalytics = FirebaseAnalytics.getInstance(this);
 		
 		toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
